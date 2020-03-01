a98713bca70f8ae09caab3747acc29a49e26fb33
linjingwen <723499747@qq.com>
2020-03-01 00:15:40 -0800

    first java

---
+++ java/Hello.java
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: Hello.java
+	> Author: 
+	> Mail: 
+	> Created Time: Sun 01 Mar 2020 12:12:33 AM PST
+ ************************************************************************/
+
+public class Hello {
+    public static void main(String args[]) {
+        System.out.println("Hello,World");
+
+    }
+
+}
+
