e9871163b8c54f8f53cdca06fb5c297d3bb13d86
linjingwen <723499747@qq.com>
2020-03-01 01:14:34 -0800

    java Function

---
+++ java/Function.java
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: Function.java
+	> Author: 
+	> Mail: 
+	> Created Time: Sun 01 Mar 2020 01:05:51 AM PST
+ ************************************************************************/
+
+public class Function {
+    public static void main(String args[]) {
+        System.out.println(add(1,2));
+    }
+    public static int add(int x, int y) {
+        return x + y;
+    }
+
+}
+
